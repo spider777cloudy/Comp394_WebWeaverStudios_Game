65aa592fcc63813838aa0d6549d6b6ef4866df7d
Sunand Shanthos Kumar <sunandmasters@gmail.com>
2024-11-12 12:26:25 -0500

    Timer
    Timer

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,15 +1,19 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine;
 
 public class GameOverManager : MonoBehaviour
 {
-    public TextMeshProUGUI gameOverText;  // Reference to the TMP text component
+    public TextMeshProUGUI gameOverText; // Reference to the TMP text component
     public string winMessage = "You Win!";
     public string loseMessage = "You Lose!";
 
     void Start()
     {
+        if (gameOverText == null)
+        {
+            Debug.LogError("GameOverManager: gameOverText is not assigned!");
+        }
+
         // Check if the player won or lost
         bool playerWon = PlayerPrefs.GetInt("PlayerWon") == 1;
 
@@ -17,22 +21,15 @@ public class GameOverManager : MonoBehaviour
         ShowGameOverScreen(playerWon);
     }
 
-    // Function to show the correct game over message
     public void ShowGameOverScreen(bool won)
     {
-        gameOverText.text = won ? winMessage : loseMessage;
-    }
-
-    // Function for the Play Again button
-    public void PlayAgain()
-    {
-        // Reload the last played level
-        SceneManager.LoadScene(PlayerPrefs.GetString("CurrentLevel"));
-    }
-
-    // Function for the Main Menu button
-    public void MainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");  // Make sure you have a MainMenu scene created
+        if (gameOverText != null)
+        {
+            gameOverText.text = won ? winMessage : loseMessage;
+        }
+        else
+        {
+            Debug.LogError("ShowGameOverScreen: gameOverText is not assigned!");
+        }
     }
 }
